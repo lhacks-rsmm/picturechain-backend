c19bcf52b1c8e4768f00577327c4f0b48cd24ff5
Matthew Philip Widjaja <matthewwidjaja@Matthews-MacBook-Air.local>
2023-09-23 11:03:22 -0700

    Added createPrompt() test in test.py

--- src/test.py
+++ src/test.py
@@ -6,6 +6,6 @@ from User import User
 def main() :
     lobbyManager = LobbyManager("") # DO NOT PUSH TO GITHUB
     lobby = lobbyManager.createLobby([User("123", "Jack")])
-    print(lobby.users)
+    print(lobby.createPrompt("123", "air guitar", "512x512"))
 
 main()
