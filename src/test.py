9ae55ea738988908288790069ba39d2b4a078d49
Matthew Philip Widjaja <matthewwidjaja@Matthews-MacBook-Air.local>
2023-09-23 10:38:57 -0700

    Added test cases

---
+++ src/test.py
@@ -0,0 +1,14 @@
+# Test cases: 
+from LobbyManager import LobbyManager
+from Lobby import Lobby
+from User import User
+
+def main() :
+    lobbyManager = LobbyManager("") # DO NOT PUSH TO GITHUB
+    lobby = lobbyManager.createLobby([User("123", "Jack")])
+    users = {}
+
+    
+
+
+main()
