632c1b753589bcdca07610a8268f0851a1497cc0
Matthew Philip Widjaja <matthewwidjaja@Matthews-MacBook-Air.local>
2023-09-23 09:53:35 -0700

    Implemented deleteLobby in LobbyManager.

--- src/LobbyManager.py
+++ src/LobbyManager.py
@@ -5,8 +5,14 @@ class LobbyManager:
     def __init__(self):
         self.lobbyMap: dict[str, Lobby]
 
-    def createLobby(users: list[User]):
+    def createLobby(self, users: list[User]):
         return Lobby(users)
 
-    def deleteLobby(id):
-        
+    def deleteLobby(self, id: str):    
+        try:
+            del self.lobbyMap[id]
+            return True
+        except KeyError as e:
+            print("Key not found")
+        return False
+
